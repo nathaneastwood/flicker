53a8be949bb005450d1e228a8822c890ec429cac
nathaneastwood <nathan.eastwood@icloud.com>
2021-02-21 18:37:05 +0100

    refactor: Move reexports from utils
    Also add extra documentation and imports

--- R/utils.R
+++ R/utils.R
@@ -1,25 +1,8 @@
-#' Replace `:::` to avoid R CMD NOTEs
-#'
-#' If we require the use of an unexported function from another package, we must access it using the triple colon
-#' (`:::`) or code the original source code into our package. R CMD check will throw a NOTE at any use of the triple
-#' colon and so this function is a simple replacement of that function.
-#'
-#' @param pkg `character(1)`. The name of the package you want to run an unexported function from
-#' @param fun `character(1)`. The name of the function you want to call
-#'
-#' @examples
-#' \dontrun{
-#'   'stats' %:::% 'Pillai'
-#' }
-#'
-#' @noRd
-`%:::%` <- function(pkg, fun) {
-  get(x = fun, envir = asNamespace(pkg), inherits = FALSE)
-}
-
-simulate_vars <- "dbplyr" %:::% "simulate_vars"
-
 #' Lazy R Object Checks
+#'
+#' @param x Any R object.
+#'
+#' @return A `logical(1)`.
 #'
 #' @export
 is.tbl_lazy <- function(x) {
